8a778a9cc576cf5c8c41e332227ab63ea7a9cd95
Huda-slo <huda1slo31@yahoo.com>
2025-10-23 17:49:19 +0300

    Initial Commit

---
+++ Meetly/MeetlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetlyApp.swift
+//  Meetly
+//
+//  Created by Huda Almadi on 23/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MeetlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
